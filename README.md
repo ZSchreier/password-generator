2b88e70ea71efcc2a512a93916a186baa22ec451
zschreier <zjschreier@gmail.com>
2023-09-28 16:47:54 -0500

    added the README.md file and re-organized the files for the github repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# **Module 3 Challenge:** Password Generator
+
+## Description
+
+The goal of this project was to create a password generator using prompt and confirm functions to set the parameters of the password. Using the responses gained from these functions, more functions were defined and utilized with the JavaScript file in order to randomly generate a password. On the developer side, several global variables were set as arrays in order to access and modify the data more easily. Ideally, this approach with global variables and multiple functions helps to highlight the flow of the process of generating the password and enhances the readability of the code.
+
+## Preview
+
+After the User selects their preferences through the prompt and confirm windows, the webpage should render the password in the provided textbox as shown below:
+
+![webpage](./password-generated.png)
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Here are my global variables that will be tweaked around as the User navigates through the various prompts on the webpage.
+
 var passLength;
 
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -22,6 +23,8 @@ var characterSelected;
 var chosenCharacter;
 var chosenPassword = [];
 
+//Here are the various helper/nesting functions that tackle the various parts of the request
+
 function generateRandomNumber(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -34,6 +37,7 @@ function giveLength(){
   }
 }
 
+// These confirm functions ask which characters the User would like to use
 function confirmLower(){
   lowChoice = confirm(`Would you like to use lowercase letters in your password?`);
 }
@@ -66,6 +70,7 @@ function optionChecker(){
   }
 }
 
+// These functions set up the chosen characters into an array, and then randomly picks them and adds them to into another array
 function optionMaker(){
   if(lowChoice){
     options.push(lowercase);
@@ -101,21 +106,28 @@ function randomPick(){
   }
 }
 
+// This is the main function that is called when the button is clicked, which runs all the previous functions and returns a joined string from the array that was randomly generated
 
+function generatePassword(){
+  // This if statement resets the global values in the case that the User wants to generate another password
 
-
-giveLength();
-confirmGrabber();
-randomPick();
-alert(`The password is ${chosenPassword.join("")}`);
-
-// console.log(lowChoice);
-// console.log(upperChoice);
-// console.log(numChoice);
-// console.log(symChoice);
-
-// console.log(options);
-
+  if(passLength !== null){
+    passLength = null;
+    lowChoice = null;
+    upperChoice = null;
+    numChoice = null;
+    symChoice = null;
+    options = [];
+    optionSelected = null;
+    characterSelected = null;
+    chosenCharacter = null;
+    chosenPassword = [];
+  }
+  giveLength();
+  confirmGrabber();
+  randomPick();
+  return chosenPassword.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
